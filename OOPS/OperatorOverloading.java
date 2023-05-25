e11e4921ae071ae6be8fec2d14034bacbaea74ce
naveen <naveenkrishnalakshmi@gmail.com>
2023-05-25 17:40:32 +0530

    operator overloading

---
+++ OOPS/OperatorOverloading.java
@@ -0,0 +1,8 @@
+public class OperatorOverloading {
+    public static void main(String args[])
+    {
+        System.out.println("java"+"programming");
+        System.out.println(2+6);
+    }
+    
+}
